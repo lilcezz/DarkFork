5b195fa3d7664aa368526e28cc95cd8d3e1c0438
darkelement1987 <jgernaat1987@gmail.com>
2018-03-21 15:57:39 +0100

    Adds 'New Spawn' to rarity notifications

--- static/js/map.js
+++ static/js/map.js
@@ -3079,7 +3079,7 @@ $(function () {
         })
         $selectRarityNotify.select2({
             placeholder: i8ln('Select Rarity'),
-            data: [i8ln('Common'), i8ln('Uncommon'), i8ln('Rare'), i8ln('Very Rare'), i8ln('Ultra Rare')],
+            data: [i8ln('Common'), i8ln('Uncommon'), i8ln('Rare'), i8ln('Very Rare'), i8ln('Ultra Rare'), i8ln('New Spawn')],
             templateResult: formatState
         })
 
