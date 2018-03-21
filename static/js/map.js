a15e0dcd0a1e62676080338200befb683d15a2db
cecpk <cec@retrospot.de>
2018-03-21 09:06:02 +0100

    Bugfix

--- static/js/map.js
+++ static/js/map.js
@@ -1264,7 +1264,8 @@ function isNotifyPerfectionPoke(poke) {
         hasHighAttributes = hasHighAttributes || shouldNotifyForLevel
     }
 
-    if ((poke['cp_multiplier'] !== null && Store.get('showMedalMagikarp') && poke['pokemon_id']==129) {
+    if (poke['cp_multiplier'] !== null) {
+    if (Store.get('showMedalMagikarp') && poke['pokemon_id']==129) {
 
         var baseHeight = 0.90
         var baseWeight = 10.00
@@ -1277,8 +1278,10 @@ function isNotifyPerfectionPoke(poke) {
             hasHighAttributes = hasHighAttributes || MedalMagikarp
         }
     }
+    }
 
-    if ((poke['cp_multiplier'] !== null && Store.get('showMedalRattata') && poke['pokemon_id']==19) {
+    if (poke['cp_multiplier'] !== null) {
+    if (Store.get('showMedalRattata') && poke['pokemon_id']==19) {
 
         var baseHeight = 0.30
         var baseWeight = 3.50
@@ -1290,6 +1293,7 @@ function isNotifyPerfectionPoke(poke) {
             MedalRattata = true
             hasHighAttributes = hasHighAttributes || MedalRattata
         }
+    }
     }
 
     return hasHighAttributes
