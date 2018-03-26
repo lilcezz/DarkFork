6dd2c15806bf219e322434e03980f35277bddfe7
darkelement1987 <jgernaat1987@gmail.com>
2018-03-27 00:30:15 +0200

    Update map.js

--- static/js/map.js
+++ static/js/map.js
@@ -24,6 +24,13 @@ var $selectSearchIconMarker
 var $selectLocationIconMarker
 var $switchGymSidebar
 var $selectExcludeRarity
+var pokemonGen = ['?'];
+for (var i = 001; i <= 151; i++) {pokemonGen.push(1);}
+for (var i = 152; i <= 251; i++) {pokemonGen.push(2);}
+for (var i = 252; i <= 386; i++) {pokemonGen.push(3);}
+// for (var i = 387; i <= 493; i++) {pokemonGen.push(4);}
+// for (var i = 494; i <= 649; i++) {pokemonGen.push(5);}
+// for (var i = 650; i <= 721; i++) {pokemonGen.push(6);}
 
 const language = document.documentElement.lang === '' ? 'en' : document.documentElement.lang
 var idToPokemon = {}
@@ -629,16 +636,7 @@ function scout(encounterId) { // eslint-disable-line no-unused-vars
 
 function pokemonLabel(item) {
     const pokemonRarity = getPokemonRarity(item['pokemon_id'])
-    var name = item['pokemon_name']
-    
-    var pokemonGen = ['?'];
-for (var i = 001; i <= 151; i++) {pokemonGen.push(1);}
-for (var i = 152; i <= 251; i++) {pokemonGen.push(2);}
-for (var i = 252; i <= 386; i++) {pokemonGen.push(3);}
-for (var i = 387; i <= 493; i++) {pokemonGen.push(4);}
-for (var i = 494; i <= 649; i++) {pokemonGen.push(5);}
-for (var i = 650; i <= 721; i++) {pokemonGen.push(6);}
-    
+    var name = item['pokemon_name']    
     var rarityDisplay = pokemonRarity ? '(' + pokemonRarity + ')' : ''
     var types = item['pokemon_types']
     var typesDisplay = ''
