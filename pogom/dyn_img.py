05c60144d15d71965314d4108d195a1ab83fa65b
darkelement1987 <jgernaat1987@gmail.com>
2018-04-18 12:49:10 +0200

    Update dyn_img.py

--- pogom/dyn_img.py
+++ pogom/dyn_img.py
@@ -133,9 +133,11 @@ def get_pokemon_map_icon(pkm, weather=None, gender=None,
         pkm_idx = pkm - 1
         x = (pkm_idx % pkm_sprites_cols) * pkm_sprites_size
         y = (pkm_idx / pkm_sprites_cols) * pkm_sprites_size
-        im_lines.append('-quality 50% -adaptive-resize 50% ' +
-                        '-crop {size}x{size}+{x}+{y} +repage' +
-                        .format(size=target_size, x=x, y=y))
+        im_lines.append(
+            '-quality 50% -adaptive-resize 50% ' +
+            '-crop {size}x{size}+{x}+{y} ' +
+            '+repage'.format(size=target_size, x=x, y=y)
+        )
 
     if weather:
         radius = 20
